97a940aaedf72eb41b0ddf32c0c4703d5b579f6a
Simona Sorlescu <simona.sorlescu@gmail.com>
2016-02-09 10:55:03 +0000

    Add profile to passport scope

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get('/searching', function(req, res){
   });
 });
 
-app.get('/auth/google', passport.authenticate('google', {scope: ['email']}));
+app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 app.get('/auth/google/callback', passport.authenticate('google', {
   successRedirect: '/',
   failureRedirect: '/fail'
