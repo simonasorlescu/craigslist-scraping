dc0d8d4d22516c054efdc1f817a6d1e0685327f0
Simona Sorlescu <simona.sorlescu@gmail.com>
2016-02-09 10:09:36 +0000

    Refactor code

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google: {
+        clientID: '60568825522-52qmtg82t11tm3pa35b7q0r39a00j5pk.apps.googleusercontent.com',
+        clientSecret: 'B8HswAPpuHR1PDmU-hvMgMKe',
+        callbackURL: "https://craigslist-scraping.herokuapp.com/auth/google/callback"
+    },
+    mongoUrl: 'mongodb://simonasorlescu:simona1@ds059375.mongolab.com:59375/craigslist'
+};
+
---
+++ models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var config = require('../config');
+
+// create a user model
+var userSchema = new mongoose.Schema({
+  name: String,
+  email: {type: String, lowercase: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
